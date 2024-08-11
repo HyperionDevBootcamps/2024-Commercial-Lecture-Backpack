e4f4cde90353804128a2f6dceee0e9ee5522f684
Julien Nyambal <163305741+JulienNyambal-hyperiondev@users.noreply.github.com>
2024-08-11 22:58:34 +0200

    SE adding week 1 lectures

---
+++ Software Engineering (SE)/Week1/Lecture2/Examples/w1l2_for_loops_example.py
@@ -0,0 +1,41 @@
+# Example 1
+fruit_list = ["apple", "orange", "peer", "litchi"]
+
+print(fruit_list)
+print("This is the list 👆")
+
+for fruit in fruit_list:
+    print("Current fruit: ", fruit)
+
+# # Example 2
+random_sent = "Hello there General Kenobi"
+
+for letter in random_sent:
+    print(letter)
+
+# Example 3
+# range(6) = [1, 2, 3, 4, 5] - natural list
+#               0 1 2 3 4  - true list as read in memory
+for number in range(1, 6):
+    print(number)
+
+for number in range(0, 10, 2):
+    print(number)
+
+# Example 4
+for i in range(1, 4):
+    # print("Outer Loop postion: ", i)
+    for j in range(1, 4):
+        # print("Inner Loop position", j)
+        print(i * j, end=" ")
+    print()
+
+
+# Example 5
+random_sent = "Well, of course I know him. He's me"
+random_sent_split = random_sent.split(" ")
+print(random_sent_split)
+
+for letter in random_sent_split:
+    print(letter)
+    
