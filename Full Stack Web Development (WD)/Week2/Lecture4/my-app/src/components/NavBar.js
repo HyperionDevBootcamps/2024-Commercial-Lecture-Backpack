3a0b3dcd8558980b334edea9a387bc7343f8dfec
Dan-Hyperion <danw@hyperiondev.com>
2024-08-14 21:51:18 +0300

    Week 2 Lesson 2

---
+++ Full Stack Web Development (WD)/Week2/Lecture4/my-app/src/components/NavBar.js
@@ -0,0 +1,19 @@
+function NavBar () {
+    return (
+        <nav>
+            <div>
+                <a>Pricing</a>
+                <a>Our Method</a>
+                <a>For patners</a>
+                <a>Contact Us</a>
+            </div>
+            <div>
+                <a>Log in</a>
+                <a>Join Us now</a>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar
